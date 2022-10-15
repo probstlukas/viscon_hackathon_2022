d67ac5845ae860fe979c23895b7458ce287816c6
Simon Sure <info@simonsure.com>
2022-10-15 11:03:57 +0200

    creating events, showing all event details

--- website/models.py
+++ website/models.py
@@ -10,8 +10,11 @@ class Note(db.Model):
 
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
+    name = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
+    location = db.Column(db.String(10000))
+    foods = db.Column(db.String(10000))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 # Anything that's not related to authentication that the user can go to, will go here (login page, home page, etc.)
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
-from .models import Note
+from .models import Note, Event
 from . import db
 import json
 
@@ -9,22 +9,31 @@ views = Blueprint('views', __name__)
 
 # Whenever we go on URL and type in /, this will show
 @views.route('/', methods=['GET', 'POST'])
-#@login_required
 def home():
-    '''if request.method == 'POST':
-        note = request.form.get('note')
+    if current_user.is_authenticated:
+        return render_template("home.html", user=current_user, events=Event.query.get())
+    else:
+        return render_template("home.html", user=current_user, events=Event.query.all())
 
-        if len(note) < 1:
+@views.route('/add-event', methods=['GET', 'POST'])
+@login_required
+def add_note():
+    if request.method == 'POST':
+        eventLocation = request.form.get('eventLocation')
+        eventName = request.form.get('eventName')
+        eventFoods = request.form.get('eventFoods')
+
+        '''if len(note) < 1:
             flash('Note is too short!', category='error')
-        else:
-            new_note = Note(data=note, user_id=current_user.id)
-            db.session.add(new_note)
-            db.session.commit()
-            flash('Note added!', category='success')'''
+        else:'''
+        new_event = Event(name=eventName, location=eventLocation, foods=eventFoods, user_id=current_user.id)
+        db.session.add(new_event)
+        db.session.commit()
+        flash('Event added!', category='success')
 
-    return render_template("home.html", user=current_user, notes=Note.query.all())
+    return render_template("add_event.html", user=current_user)
 
-@views.route('/delete-note', methods=['POST'])
+'''@views.route('/delete-note', methods=['POST'])
 def delete_note():
     note = json.loads(request.data)
     noteId = note['noteId']
@@ -34,4 +43,4 @@ def delete_note():
             db.session.delete(note)
             db.session.commit()
 
-    return jsonify({})
+    return jsonify({})'''
