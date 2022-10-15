94c15610d91fe36eb45997b21eaf7dc8bec694d2
Simon Sure <info@simonsure.com>
2022-10-15 16:58:36 +0200

    adding image view to main page

--- website/views.py
+++ website/views.py
@@ -21,11 +21,11 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 @views.route('/', methods=['GET', 'POST'])
 def home():
     if current_user.is_authenticated:
-        return render_template("home.html", user=current_user, events=Event.query.filter_by(user_id=current_user.id,
+        return render_template("home.html", base_url=(url_for('views.home')+'img'+'/'), image=Image, user=current_user, events=Event.query.filter_by(user_id=current_user.id,
                                                                                             visibility=True)
                                .filter(Event.date < Event.endDate).order_by(Event.date).all())
     else:
-        return render_template("home.html", user=current_user, events=Event.query.filter_by(visibility=True)
+        return render_template("home.html", base_url=url_for('views.image_folder'), image=Image, user=current_user, events=Event.query.filter_by(visibility=True)
                                .filter(Event.date < Event.endDate).order_by(Event.date).all())
 
 
@@ -71,32 +71,9 @@ def add_event():
         flash('Event added!', category='success')
     return render_template("add_event.html", user=current_user)
 
-
-'''
-@views.route('/', methods=['POST'])
-def upload_image():
-    if 'file' not in request.files:
-        flash('No file part', category='error')
-        return redirect(request.url)
-    file = request.files['file']
-    if file.filename == '':
-        flash('No image selected for uploading', category='error')
-        return redirect(request.url)
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        flash('Photo successfully uploaded', category='success')
-        return render_template('add_event.html', filename=filename)
-    else:
-        flash('Allowed image types are: png, jpg, jpeg, gif', category='error')
-        return redirect(request.url)
-'''
-
-
 @views.route('/img/<filename>')
 def display_image(filename):
     return send_file(path.join(app.instance_path, app.config['UPLOAD_FOLDER'], filename), mimetype='image/gif')
-
 
 @views.route('/delete-event', methods=['POST'])
 def delete_event():
