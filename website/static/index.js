09717f6c24832b01c6cc55b5f9b44528e57392f1
Simon Sure <info@simonsure.com>
2022-10-16 09:07:17 +0200

    preparing addInterest increase and portion change

--- website/static/index.js
+++ website/static/index.js
@@ -6,3 +6,28 @@ function deleteEvent(eventId) {
       window.location.href = "/";
     });
 }
+
+function addInterest(eventId, foodName) {
+    console.log('eventId: ' + eventId + '; foodName: '+foodName)
+    fetch("/add-interest", {
+      method: "POST",
+      body: JSON.stringify({ eventId: eventId, foodName: foodName }),
+    }).then((_res) => {
+        window.location.href = "/";
+    });
+}
+
+function saveChanges(eventId, foodName) {
+    let newQuantity = document.getElementById(eventId+foodName).value
+    console.log('eventId: ' + eventId + '; foodName: '+foodName+'; newQuantity: '+newQuantity)
+    if (newQuantity=='') {
+        console.log('escaping')
+        return null
+    }
+    fetch("/update-event", {
+      method: "POST",
+      body: JSON.stringify({ eventId: eventId, foodName: foodName, newQuantity: parseInt(newQuantity) }),
+    }).then((_res) => {
+      window.location.href = "/user-events";
+    });
+}
